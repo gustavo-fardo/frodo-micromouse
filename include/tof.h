5df5b0191557458c095b47c1bb2aa9c46c9ac5c4
Lucas Walger <walger.profissional@gmail.com>
2024-04-25 12:26:16 -0300

    tof functions

---
+++ include/tof.h
@@ -0,0 +1,11 @@
+#ifndef _TOF
+#define _TOF
+
+float dist_left, dist_right, dist_front_left, dist_front_right;
+bool wall_left,wall_front,wall_right;
+
+void setupTOF();
+void readTOF();
+
+
+#endif
---
+++ src/tof.cpp
@@ -0,0 +1,56 @@
+#include <tof.h>
+#include <VL53L0X.h>
+#include <Wire.h>
+#include <constants.h>
+#include <pins.h>
+VL53L0X sensor_left;
+VL53L0X sensor_front_left;
+VL53L0X sensor_front_right;
+VL53L0X sensor_right;
+void readTOF()
+{
+    dist_left = sensor_left.readRangeContinuousMillimeters()*0.1f;
+    dist_right = sensor_right.readRangeContinuousMillimeters()*0.1f;
+    dist_front_left = sensor_front_left.readRangeContinuousMillimeters()*0.1f;
+    dist_front_right = sensor_front_right.readRangeContinuousMillimeters()*0.1f;
+
+    wall_front = (dist_front_left+dist_front_right <= FRONT_WALL_DIST*2) ? true : false;
+    wall_left = (dist_left <= SIDE_WALL_DIST) ? true: false;
+    wall_right = (dist_right<= SIDE_WALL_DIST) ? true: false;
+}
+
+void setupTOF()
+{
+    Wire.begin();
+    pinMode(XSHUT1,OUTPUT);
+    pinMode(XSHUT2,OUTPUT);
+    pinMode(XSHUT3,OUTPUT);
+    digitalWrite(XSHUT1,LOW);
+    digitalWrite(XSHUT2,LOW);
+    digitalWrite(XSHUT3,LOW);
+    delay(10);
+    sensor_left.setAddress(0x10);
+    digitalWrite(XSHUT1,HIGH);
+    delay(10);
+    sensor_front_left.setAddress(0x20);
+    digitalWrite(XSHUT2,HIGH);
+    delay(10);
+    sensor_front_right.setAddress(0x30);
+    digitalWrite(XSHUT3,HIGH);
+    delay(10);
+    sensor_right.setAddress(0x40);
+    sensor_left.init();
+    sensor_right.init();
+    sensor_front_left.init();
+    sensor_front_right.init();
+    
+    sensor_left.setTimeout(500);
+    sensor_right.setTimeout(500);
+    sensor_front_left.setTimeout(500);
+    sensor_front_right.setTimeout(500);
+
+    sensor_left.startContinuous();
+    sensor_right.startContinuous();
+    sensor_front_left.startContinuous();
+    sensor_front_right.startContinuous();
+}
