b3fc6b48ad58f85d57dce1e01242b32d04ce9ca2
Lucas Walger <walger.profissional@gmail.com>
2024-04-30 21:57:41 -0300

    Atualizado tof.h com documentação e alterações nas posições de constantes

--- include/tof.h
+++ include/tof.h
@@ -1,10 +1,38 @@
+/* @name TOF
+* @author @walger-lucas
+* @date 30/04/2024
+* @paragraph Funções para interface geral com os TOFs.
+*/
 #ifndef _TOF
 #define _TOF
 #include <Arduino.h>
+
+// WALL DISTANCE OFFSET CONSTANTS (mm)
+#define TOF_OFFSET 10
+
+//variáveis que retornam as saídas dos sensores em mm, respectivamente esquerda, direita, frente esquerda e frente direita
 extern int16_t dist_left, dist_right, dist_front_left, dist_front_right;
+//bools que retornam caso foi percebido uma parede, respectivamente, a esquerda, a frente e a direita
 extern bool wall_left,wall_front,wall_right;
 
+
+/* @name setupTOF
+* @brief Faz setup do I2C e separação entre os sensores TOF, além de inicializá-los.
+* @return void
+* @date alterações:
+*   - 30/04/2024: criado comentário, começado a documentar. - @walger-lucas
+*/
 void setupTOF();
+
+
+/* @name readTOF
+* @brief Atualiza variáveis globais dist_left, dist_right, dist_front_left e dist_front_right com os dados sáidos dos sensores. 
+* Também atualiza flags wall_left, wall_front e wall_right, a partir da distância para cada uma dessas direções e as constantes em constants.h
+* @return void
+* @attention todos os valores dos sensores possuem seu valor diminuido em TOF_OFFSETS
+* @date alterações:
+*   - 30/04/2024: criado comentário, começado a documentar. - @walger-lucas
+*/
 void readTOF();
 
 
--- src/tof.cpp
+++ src/tof.cpp
@@ -7,10 +7,13 @@
 int16_t dist_left=0, dist_right=0, dist_front_left=0, dist_front_right=0;
 bool wall_left=0,wall_front=0,wall_right=0;
 
+//estruturas que guardam valor de medição dos sensores
 VL53L0X sensor_left;
 VL53L0X sensor_front_left;
 VL53L0X sensor_front_right;
 VL53L0X sensor_right;
+
+
 void readTOF()
 {
     dist_left = sensor_left.readRangeContinuousMillimeters()- TOF_OFFSET;
@@ -25,13 +28,14 @@ void readTOF()
 
 void setupTOF()
 {
-    Wire.begin();
+    Wire.begin(); // inicializa I2C
     pinMode(XSHUT1,OUTPUT);
     pinMode(XSHUT2,OUTPUT);
     pinMode(XSHUT3,OUTPUT);
     digitalWrite(XSHUT1,LOW);
     digitalWrite(XSHUT2,LOW);
     digitalWrite(XSHUT3,LOW);
+    //ativa sensor 1 por 1, mudando seu endereço original
     delay(10);
     sensor_left.setAddress(0x10);
     digitalWrite(XSHUT1,HIGH);
@@ -43,16 +47,19 @@ void setupTOF()
     digitalWrite(XSHUT3,HIGH);
     delay(10);
     sensor_right.setAddress(0x40);
+    //inicializa os sensores
     sensor_left.init();
     sensor_right.init();
     sensor_front_left.init();
     sensor_front_right.init();
     
+    //seta timeout para os sensores
     sensor_left.setTimeout(500);
     sensor_right.setTimeout(500);
     sensor_front_left.setTimeout(500);
     sensor_front_right.setTimeout(500);
 
+    //seta modo de leitura continua
     sensor_left.startContinuous();
     sensor_right.startContinuous();
     sensor_front_left.startContinuous();
